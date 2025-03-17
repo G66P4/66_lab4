549d16c1a1ed3b91aa791eeee6e586a7b7f8311c
MatiasTorres <matiastowers123@gmail.com>
2025-03-17 15:46:04 -0400

    Paginaweb

--- include/PaginaWeb.h
+++ include/PaginaWeb.h
@@ -1,1 +1,17 @@
+#ifndef PAGINAWEB
+#define PAGINAWEB 
+#include <iostream>
+#include "Publicacion.h"
+#include "DTFecha.h"
+class PaginaWeb: public Publicacion{
+	private:
+		std::string url;
+		std::string contenidoExtraido;
+	public:
+		PaginaWeb(std::string,std::string,DTFecha,std::string,std::string);
+		std::string getUrl();
+		std::string getContenidoExtraido();
+		bool contienePalabra(std::string);
+};
 
+#endif
--- src/PaginaWeb.cpp
+++ src/PaginaWeb.cpp
@@ -1,1 +1,22 @@
+#include "../include/PaginaWeb.h"
+#include <iostream>
 
+
+//constructor
+PaginaWeb::PaginaWeb(std::string doi,std::string tit, DTFecha f,std::string url,std::string contenidoExtraido):Publicacion(doi,tit,f){
+	this->url=url;
+	this->contenidoExtraido=contenidoExtraido;
+}
+
+//getters
+std::string PaginaWeb::getUrl(){
+	return url;
+}
+
+std::string PaginaWeb::getContenidoExtraido(){
+	return contenidoExtraido;
+}
+
+bool PaginaWeb::contienePalabra(std::string palabra){
+	return contenidoExtraido.find(palabra) != std::string::npos;
+}
