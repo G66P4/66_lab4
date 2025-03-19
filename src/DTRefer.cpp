9c9191384d9ed4b90aa14823694cffd4eb351623
MatiasTorres <matiastowers123@gmail.com>
2025-03-19 16:53:59 -0400

    DTREFER

--- src/DTRefer.cpp
+++ src/DTRefer.cpp
@@ -1,1 +1,29 @@
+#include "../include/DTRefer.h"
+
+//constructor
+DTRefer::DTRefer(std::string doi, std::string titulo, DTFecha fecha,std::set<std::string> autores){
+	this->DOI=doi;
+	this->Titulo=titulo;
+	this->Fecha=fecha;
+	this->Autores=autores;
+
+}
+
+std::ostream &operator<<(std::ostream &o, DTRefer &refer) {
+	o << refer.DOI << "->" << refer.Titulo << "(" << refer.Fecha.get_dia() << "/" <<refer.Fecha.get_mes() << "/" <<refer.Fecha.get_anio() << ")/";
+	
+	std::set<std::string>::const_iterator inicio= refer.Autores.begin();
+	std::set<std::string>::const_iterator  final= refer.Autores.end();
+    
+	if(inicio!=final){
+		o << *inicio ;
+		inicio++;
+	}
+	
+	while(inicio!=final){
+		o << ", " << *inicio;
+		inicio++;
+	}
+	return o;
+}
 
