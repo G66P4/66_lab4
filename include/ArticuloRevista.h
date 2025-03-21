4fb456ae5c79c1453dd31f3797aadb4c65781e99
Eemi02 <emiesponda2005@gmail.com>
2025-03-21 11:17:50 -0300

    ArticuloRevistaProbado

--- include/ArticuloRevista.h
+++ include/ArticuloRevista.h
@@ -1,1 +1,19 @@
+#ifndef ARTICULOREVISTA
+#define ARTICULOREVISTA
+#include <iostream>
+#include "Publicacion.h"
+#include "DTFecha.h"
 
+class ArticuloRevista: public Publicacion{
+    private:
+        std::string revista;
+        std::string extracto; 
+    public:
+        ArticuloRevista(std::string, std::string, DTFecha, std::string, std::string);
+        ~ArticuloRevista();
+        std::string getRevista();
+        std::string getExtracto();
+        virtual bool contienePalabra(std::string);
+};
+
+#endif
--- src/ArticuloRevista.cpp
+++ src/ArticuloRevista.cpp
@@ -1,1 +1,25 @@
+#include "../include/ArticuloRevista.h"
+#include <iostream>
 
+//constructor
+ArticuloRevista::ArticuloRevista(std::string doi, std::string tit, DTFecha f,std::string revista, std::string extracto):Publicacion(doi, tit, f){
+    this->revista = revista;
+    this->extracto = extracto;
+};
+
+//destructor
+ArticuloRevista::~ArticuloRevista(){
+};
+
+//getters
+std::string ArticuloRevista::getRevista(){
+    return revista;
+}
+std::string ArticuloRevista::getExtracto(){
+    return extracto;
+}
+
+bool ArticuloRevista:: contienePalabra(std::string palabra){
+    bool b = extracto.find(palabra) != std::string::npos;
+    return b;
+};
