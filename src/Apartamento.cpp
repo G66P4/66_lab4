a494a98bb9487622c35b83301181537e66694e11
tomy3555 <tomilagomar@gmail.com>
2025-06-05 10:58:14 -0300

    apto terminado(en principio)

---
+++ src/Apartamento.cpp
@@ -0,0 +1,25 @@
+#include "../include/Apartamento.h"
+#include <iostream>
+
+using namespace std;
+
+Apartamento::Apartamento(int codigo, std::string direccion, int numeroPuerta, int superficie,
+                         int anoConstruccion, int piso, bool tieneAscensor, float gastosComunes)
+    : Inmueble(codigo, direccion, numeroPuerta, superficie, anoConstruccion),
+      piso(piso), tieneAscensor(tieneAscensor), gastosComunes(gastosComunes) {}
+
+// Destructor
+Apartamento::~Apartamento() {}
+
+// Getters
+int Apartamento::getPiso() const {
+    return piso;
+}
+
+bool Apartamento::getTieneAscensor() const {
+    return tieneAscensor;
+}
+
+float Apartamento::getGastosComunes() const {
+    return gastosComunes;
+}
