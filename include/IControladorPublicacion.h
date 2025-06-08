5d3af6821d59ea192b77433c32f8c4a86473a762
MatiasTorres <matiastowers123@gmail.com>
2025-06-08 13:26:42 -0400

    interfaz controlador publicacion

---
+++ include/IControladorPublicacion.h
@@ -0,0 +1,16 @@
+#ifndef ICONTROLADORPUBLICACION_H
+#define ICONTROLADORPUBLICACION_H
+#include <string>
+#include <set>
+#include "Publicacion.h"
+#include "DTPublicacion.h"
+#include "TipoPublicacion.h"
+#include "TipoInmueble.h"
+
+class IControladorPublicacion {
+    public:
+        virtual std::set<DTPublicacion*> listarPublicacion(TipoPublicacion tipoP,float precioMinimo,float precioMaximo,TipoInmueble tipoI) = 0;
+        virtual bool altaPublicacion(std::string nicknameInmobiliaria, int codigoInmueble, TipoPublicacion tipoP, std::string texto, float precio) = 0;
+};      
+
+#endif
