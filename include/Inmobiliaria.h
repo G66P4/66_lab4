915eceb008b4af76d7207934398424caf4e7f1af
tomy3555 <tomilagomar@gmail.com>
2025-06-10 18:25:08 -0300

    inmobiliaria.h

--- include/Inmobiliaria.h
+++ include/Inmobiliaria.h
@@ -1,23 +1,40 @@
 #ifndef INMOBILIARIA_H
 #define INMOBILIARIA_H
+
 #include "Usuario.h"
 #include "DTUsuario.h"
-#include "DTInmuebleAdministrado.h"
+#include "AdministraPropiedad.h"
+
 #include <string>
+#include <map>
 #include <set>
 
 class Inmobiliaria : public Usuario {
-    private:
-        std::string direccion;
-        std::string url;
-        std::string telefono;
+private:
+    std::string direccion;
+    std::string url;
+    std::string telefono;
 
-    public:
-        Inmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion, std::string url, std::string telefono);
-        std::set<DTInmuebleAdministrado*> obtenerInmuebleData();
-        DTUsuario* getInmobiliariaData();
-        void representarPropietario(std::string nicknamePropietario);//agrega el propietario a la lista de propietarios representados
-        ~Inmobiliaria();
+    std::map<int, AdministraPropiedad*> propiedades;
+
+public:
+    Inmobiliaria(std::string nickname, std::string nombre, std::string email,
+                 std::string direccion, std::string url, std::string telefono);
+
+    // Getters
+    std::string getDireccion() const;
+    std::string getUrl() const;
+    std::string getTelefono() const;
+
+    DTUsuario obtenerInmuebleData() const;
+    AdministraPropiedad* adminPropFind(int codigoInmueble);
+    void eliminarLinkAdmProp(int codigoInmueble);
+    DTUsuario getInmobiliariaData() const;
+
+    void agregarSuscripcion();     
+    void eliminarSuscripcion();    
+
+    void modificar(); 
 };
 
 #endif
