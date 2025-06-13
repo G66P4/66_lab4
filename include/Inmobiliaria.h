c0a8cb7e49b153a662ca40a1b1250c90b5161dcd
tomy3555 <tomilagomar@gmail.com>
2025-06-13 11:21:05 -0300

    errores

--- include/Inmobiliaria.h
+++ include/Inmobiliaria.h
@@ -6,6 +6,7 @@
 #include "Propietario.h"
 #include "DTInmuebleAdministrado.h"
 #include "DTInmuebleListado.h"
+#include "Publicacion.h"
 
 #include <string>
 #include <map>
@@ -27,14 +28,14 @@ private:
 
 public:
     Inmobiliaria(std::string nickname,std::string contasena , std::string nombre, std::string email, std::string direccion, std::string url, std::string telefono);
-    Inmobiliaria::~Inmobiliaria();
+    ~Inmobiliaria();
 
     // Getters
     std::string getDireccion() const;
     std::string getUrl() const;
     std::string getTelefono() const;
 
-    std::set<DTInmuebleListado*> Inmobiliaria::listarInmueblesNoAdministrados();
+    std::set<DTInmuebleListado*> listarInmueblesNoAdministrados();
     std::set<DTInmuebleAdministrado*> obtenerInmuebleData();//devuelve todos los inmuebles administrados por la inmobiliaria
     bool agregarInmuebleAdministrado(Inmueble* inmueble);
     AdministraPropiedad* adminPropFind(int codigoInmueble);
