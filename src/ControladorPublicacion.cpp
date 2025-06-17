8bfc03ba4e25bfa06a928603e9edb5b5659a1cdf
Valen180-4 <161951550+Valen180-4@users.noreply.github.com>
2025-06-17 12:28:23 -0300

    Update ControladorPublicacion.cpp

--- src/ControladorPublicacion.cpp
+++ src/ControladorPublicacion.cpp
@@ -52,40 +52,18 @@ bool ControladorPublicacion::altaPublicacion(std::string nicknameInmobiliaria, i
     if (!(admiProp->tienePub(fechaActual, tipoPublicacion)))
     {
         //Creo una nueva publicacion
-        std::set<Publicacion *> publicaciones = admiProp->getPublicaciones();
-        Publicacion *fechaMasReciente = NULL;
-        for (std::set<Publicacion*>::iterator it = publicaciones.begin(); it != publicaciones.end(); ++it) {
-            Publicacion* pub = *it;
-            // Verificamos si es de tipo Alquiler usando dynamic_cast
-            if(fechaMasReciente != NULL && pub->getTipo() == tipoPublicacion && pub->getFecha() >= fechaMasReciente->getFecha())
-            {
-                fechaMasReciente = pub;
-            }else if (fechaMasReciente == NULL && pub->getTipo() == tipoPublicacion)
-            {
-                fechaMasReciente = pub;
-            }
-        }
-        
-        bool activa;
-        if (fechaMasReciente != NULL && fechaMasReciente->getFecha() >= fechaActual) {
-            activa = false; // Si la fecha mas reciente es mayor a la actual, la nueva publicacion no esta activa
-        } else {
-            if (fechaMasReciente != NULL){fechaMasReciente->setActiva(false); } // Desactivamos la fecha mas reciente
-            activa = true; // Si no hay publicaciones o la fecha mas reciente es menor a la actual, la nueva publicacion esta activa
-        }
-      
-        Publicacion *publicacion = new Publicacion(codigoInmueble, fechaActual, tipoPublicacion, texto, precio, activa);
+        Publicacion *publicacion = new Publicacion(codigoInmueble, fechaActual, tipoPublicacion, texto, precio, true);
         publicacion->setAdminProp(admiProp);
         //Agrego la publicacion al administr propiedad
         admiProp->agregarPub(publicacion);
         //Agrego la publicacion al manejador de publicaciones
         ManejadorPublicacion *manejadorPub = ManejadorPublicacion::getInstancia(); 
         manejadorPub->agregarPublicacion(publicacion);
-        inmo->modificar(publicacion); // Notifico a la inmobiliaria que se ha creado una nueva publicacion
         return true; 
     }
     else
     {
+        delete fechaActual;
         return false;
     }
 };
